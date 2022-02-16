6802bf892d84d779b41a0542dad46728482e51d6
Qs-F <qsf@de-liker.com>
2022-02-17 00:49:29 +0900

    Ref to move walk func into walk.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-const walk = async <StackNode>(
-  root: SceneNode,
-  f: (node: SceneNode, stack?: StackNode[]) => boolean | Promise<boolean>,
-  stack?: StackNode[]
-) => {
-  if (!(await f(root, stack))) {
-    return
-  }
-  if (!('children' in root)) {
-    return
-  }
-  await Promise.all(
-    root.children.map(async (child) => {
-      await walk(child, f)
-    })
-  )
-}
-
 const main = async () => {
   figma.showUI(__html__, { visible: false })
   figma.closePlugin('Done ✨')
